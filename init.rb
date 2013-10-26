57a1d85fe872758b249a885286e7af3ed6a31995
ande3577 <ande3577@gmail.com>
2013-10-26 14:58:07 -0500

    Add new link to top menu

--- init.rb
+++ init.rb
@@ -2,6 +2,9 @@ Redmine::Plugin.register :redmine_redirect_help do
   
   settings :default => {'help_project' => '', 'help_wiki_page' => ''}, :partial => 'settings/redirect_help_settings'
   
+  delete_menu_item :top_menu, :help  
+  menu :top_menu, :help, { :controller => 'help', :action => 'show' }, :last => true
+  
   name 'Redmine Redirect Help plugin'
   author 'David Anderson'
   description 'Redirects the help link at the top of the page to a wiki page'
