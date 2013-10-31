2ec6e18b5c747f80cc23f763c946adfe0ac51997
ande3577 <ande3577@gmail.com>
2013-10-31 18:37:28 -0500

    Checking for wiki page redirect

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -20,16 +20,19 @@ class HelpController < ApplicationController
     project_id = Setting.plugin_redmine_redirect_help['help_project']
     return redirect_to(Redmine::Info.help_url) if project_id.nil? or project_id.empty?
     @help_project = Project.find(project_id)
-    return render_404 if @help_project.wiki.nil?
+    @wiki = @help_project.wiki
+    return render_404 if @wiki.nil?
   end
   
   def get_default_page_id
     params[:id] = Setting.plugin_redmine_redirect_help['help_wiki_page']
+    return render_404 if params[:id].nil? or params[:id].empty?
   end
   
   def get_wiki_page
-    @page = @help_project.wiki.pages.where(:title => params[:id]).first
+    @page = @wiki.find_page(params[:id])
     return render_404 if @page.nil?
+    return redirect_to params.update(:id => @page.title) if @wiki.page_found_with_redirect?
     @content = @page.content
   end
 end
